8f6e5cf90963167b1c9f2292271e6bda47a52062
Nachy5 <55293421+Nachy5@users.noreply.github.com>
2021-05-15 13:09:24 +0100

    Updated python file

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-#Nothing serious
+#Nothing serious bla bla
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Vic')
+    print_hi('VC')
 
 
