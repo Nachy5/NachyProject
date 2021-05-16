4fe76d4be7ef83bdbad058a263d49f0d29b1c65f
Nachy5 <55293421+Nachy5@users.noreply.github.com>
2021-05-16 11:46:01 +0100

    Updated python index

--- main.py
+++ main.py
@@ -7,10 +7,21 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('bro')
+    print (2**4)
+    print (18%6)
+print(2*5**2)
+growth_multiplier = 1.1
+savings = 100
+result=savings*growth_multiplier**7
+print(result)
+Desc= 'compound interest'
+profitable=True
+Year1= growth_multiplier*savings
+print(type(Year1))
+doubledesc= desc+desc
+print(doubledesc)
 
 
