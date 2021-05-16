9d4af2724b69de3574d6a06603f6650535e2cd8b
Nachy5 <55293421+Nachy5@users.noreply.github.com>
2021-05-16 13:26:46 +0100

    update python index

--- main.py
+++ main.py
@@ -7,7 +7,6 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-<<<<<<< HEAD
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('bro')
@@ -18,11 +17,15 @@ growth_multiplier = 1.1
 savings = 100
 result=savings*growth_multiplier**7
 print(result)
-Desc= 'compound interest'
+Desc='compound interest'
 profitable=True
 Year1= growth_multiplier*savings
-#print type
 print(type(Year1))
-doubledesc= desc+desc
-print(doubledesc)
-
+Doubledesc= Desc+Desc
+print(Doubledesc)
+#Joining Multiple values
+#You have to convert them to strings str() or floats Float() or integers int()
+print(int(result) + int(savings))
+print("I started with $") + str(savings) + "and now have $" +str(result) + "Awesome!")
+pi_string= "3.1415926"
+pi_float= float(pi_string)
